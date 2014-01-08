75b606440e0bcecc7438ccf96da7945064b49f6c
brobits <mlane@connamara.com>
2014-01-08 15:00:25 -0600

    added support for data dictionary, ignore length and checksum tags, and last fix

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,6 +8,24 @@ require 'fix_spec/builder'
 require 'protobuf_spec/cucumber'
 require 'protobuf_spec/builder'
 
+module FIXMessageCache
+    def last_fix
+        @last_fix
+    end
+end
+
+Around('@with_data_dictionary') do |scenario, block|
+  FIXSpec::data_dictionary= FIXSpec::DataDictionary.new "features/support/FIX42.xml"
+  block.call
+  FIXSpec::data_dictionary= nil
+end
+
+Around('@ignore_length_and_checksum') do |scenario, block|
+  JsonSpec.excluded_keys=%w(CheckSum BodyLength)
+  block.call
+  JsonSpec.excluded_keys=JsonSpec::Configuration::DEFAULT_EXCLUDED_KEYS
+end
+
 FIXSpec::data_dictionary= FIXSpec::DataDictionary.new "features/support/FIX42.xml"
 
 # ignore the comparison of dynamic fields within json fix messages
